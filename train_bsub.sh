9ad3263f11a14a0ff83edd22d3347f4092ca1b4e
Filip Ginter <ginter@cs.utu.fi>
2016-05-17 17:01:05 +0300

    Taito sbatch job shell

---
+++ train_bsub.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p gpu
+#SBATCH -t 24:00:00
+#SBATCH -J fg1
+#SBATCH -o fg1.out
+#SBATCH -e fg1.err
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000
+
+source $USERAPPL/activate_gpu.sh
+# run your script
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python train.py
+deactivate
