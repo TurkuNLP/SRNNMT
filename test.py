d9daa2787cd19d230e30a1e8901cb1c605af70a0
Jenna Kanerva <jmnybl@utu.fi>
2016-08-18 09:30:04 +0300

    easier model loading

--- test.py
+++ test.py
@@ -14,7 +14,13 @@ import data_dense
 with open("keras_model.json","r") as f:
     trained_model=model_from_json(f.read())
     trained_model.load_weights("keras_weights.h5")
-
+    trained_model.layers.pop() # remove cosine and flatten layers
+    trained_model.layers.pop()
+    trained_model.outputs = [trained_model.get_layer('src_gru').output,trained_model.get_layer('src_gru').output] # define new outputs
+    trained_model.layers[-1].outbound_nodes = [] # not sure if we need these...
+    trained_model.layers[-2].outbound_nodes = []
+    print(trained_model.summary())
+    print(trained_model.outputs)
 
 #Read vocabularies
 src_f_name="data/JRC-Acquis.en-fi.fi"
@@ -29,31 +35,29 @@ gru_width=50
 
 ms=data_dense.Matrices(minibatch_size,max_sent_len)
 
-#Input: sequences representing the source and target sentences
-#Inputs
-src_inp=Input(shape=(max_sent_len,), name="source_chars", dtype="int32")
-trg_inp=Input(shape=(max_sent_len,), name="target_chars", dtype="int32")
-#Embeddings
-src_emb=Embedding(len(vs.source_chars), vec_size, input_length=max_sent_len, mask_zero=True, weights=trained_model.get_layer('src_embedding').get_weights())
-trg_emb=Embedding(len(vs.target_chars), vec_size, input_length=max_sent_len, mask_zero=True, weights=trained_model.get_layer('trg_embedding').get_weights())
-#Vectors
-src_vec=src_emb(src_inp)
-trg_vec=trg_emb(trg_inp)
-#RNNs
-src_gru=GRU(gru_width, weights=trained_model.get_layer('src_gru').get_weights())
-trg_gru=GRU(gru_width, weights=trained_model.get_layer('trg_gru').get_weights())
-src_gru_out=src_gru(src_vec)
-trg_gru_out=trg_gru(trg_vec)
-#Dense on top
-src_dense=Dense(gru_width, weights=trained_model.get_layer('src_dense').get_weights())
-trg_dense=Dense(gru_width, weights=trained_model.get_layer('trg_dense').get_weights())
-src_dense_out=src_dense(src_gru_out)
-trg_dense_out=trg_dense(trg_gru_out)
-# ready...
-
-
-model=Model(input=[src_inp,trg_inp], output=[src_dense_out,trg_dense_out])
-model.compile(optimizer='adam',loss='mse')
+###Input: sequences representing the source and target sentences
+###Inputs
+##src_inp=Input(shape=(max_sent_len,), name="source_chars", dtype="int32")
+##trg_inp=Input(shape=(max_sent_len,), name="target_chars", dtype="int32")
+###Embeddings
+##src_emb=Embedding(len(vs.source_chars), vec_size, input_length=max_sent_len, mask_zero=True, weights=trained_model.get_layer('src_embedding').get_weights())
+##trg_emb=Embedding(len(vs.target_chars), vec_size, input_length=max_sent_len, mask_zero=True, weights=trained_model.get_layer('trg_embedding').get_weights())
+###Vectors
+##src_vec=src_emb(src_inp)
+##trg_vec=trg_emb(trg_inp)
+###RNNs
+##src_gru=GRU(gru_width, weights=trained_model.get_layer('src_gru').get_weights())
+##trg_gru=GRU(gru_width, weights=trained_model.get_layer('trg_gru').get_weights())
+##src_gru_out=src_gru(src_vec)
+##trg_gru_out=trg_gru(trg_vec)
+###Dense on top
+##src_dense=Dense(gru_width, weights=trained_model.get_layer('src_dense').get_weights())
+##trg_dense=Dense(gru_width, weights=trained_model.get_layer('trg_dense').get_weights())
+##src_dense_out=src_dense(src_gru_out)
+##trg_dense_out=trg_dense(trg_gru_out)
+### ready...
+##model=Model(input=[src_inp,trg_inp], output=[src_dense_out,trg_dense_out])
+##model.compile(optimizer='adam',loss='mse')
 
 
 test_size=1000
@@ -69,18 +73,18 @@ src_data=np.zeros((test_size,gru_width))
 trg_data=np.zeros((test_size,gru_width))
 
 for i,(mx,targets) in enumerate(data_dense.fill_batch(1,max_sent_len,vs,iter_test_data(inf_iter))):
-    src,trg=model.predict(mx)
+    src,trg=trained_model.predict(mx)
     src_data[i]=src[0]
     trg_data[i]=trg[0]
     if i>test_size:
         break
 
-sims=trg_data.dot(src_data[0])
+sims=trg_data.dot(src_data[1])
 print(sims)
 print(np.dot(src_data[0],trg_data[1]))
 N=10
 results=sorted(((sims[idx],idx,inf_iter.data[idx][1]) for idx in np.argpartition(sims,-N-1)[-N-1:]), reverse=True)
-print(inf_iter.data[0][0])
+print(inf_iter.data[1][0])
 for sim,idx,text in results:
     print(idx,sim,text)
 sys.exit()
