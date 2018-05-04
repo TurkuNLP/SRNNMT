f4fb36c65ea7f82e17a3b53dfcef9273c882cced
Jenna Kanerva <jmnybl@utu.fi>
2018-05-04 11:35:54 +0300

    print devel loss and output

--- train_encoder.py
+++ train_encoder.py
@@ -23,7 +23,7 @@ import tensorflow as tf
 ### Only needed for me, not to block the whole GPU, you don't need this stuff
 from keras.backend.tensorflow_backend import set_session
 config = tf.ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
 set_session(tf.Session(config=config))
 ### ---end of weird stuff
 
@@ -38,19 +38,38 @@ def evaluate_one(inp,model,inverse_1, inverse_2,note):
 def evaluate_all(vs, model, data):
 
     # unpack data
-    (mono_src_input, mono_src_output), (mono_trg_input, mono_trg_output), (src_input, trg_input, src_output, trg_output) = data
+#    (mono_src_input, mono_src_output), (mono_trg_input, mono_trg_output), (src_input, trg_input, src_output, trg_output) = data
+    (_, _), (_, _), (src_input, trg_input, src_output, trg_output) = data
 #    inversed_source={v:k for k,v in vs.source_vocab.items()}
 #    inversed_target={v:k for k,v in vs.target_vocab.items()}
-    evaluate_one(mono_src_input, model.source_to_source_model, vs.inversed_vectorizer_source, vs.inversed_vectorizer_source, "fi autoencode crawl")
+#    evaluate_one(mono_src_input, model.source_to_source_model, vs.inversed_vectorizer_source, vs.inversed_vectorizer_source, "fi autoencode crawl")
     evaluate_one(src_input, model.source_to_source_model, vs.inversed_vectorizer_source, vs.inversed_vectorizer_source, "fi autoencode parallel")
-    evaluate_one(mono_trg_input, model.target_to_target_model, vs.inversed_vectorizer_target, vs.inversed_vectorizer_target, "en autoencode crawl")
+#    evaluate_one(mono_trg_input, model.target_to_target_model, vs.inversed_vectorizer_target, vs.inversed_vectorizer_target, "en autoencode crawl")
     evaluate_one(trg_input, model.target_to_target_model, vs.inversed_vectorizer_target, vs.inversed_vectorizer_target, "en autoencode parallel")
     evaluate_one(src_input, model.source_to_target_model, vs.inversed_vectorizer_source, vs.inversed_vectorizer_target, "fi-->en")
     evaluate_one(trg_input, model.target_to_source_model, vs.inversed_vectorizer_target, vs.inversed_vectorizer_source, "en-->fi")
 
-        
 
-    
+def calculate_devel_loss(model, data, args):
+    (_, _), (_, _), (src_input, trg_input, src_output, trg_output) = data
+    loss={"_":[], "_":[], "parallel_source_autoencode":[], "parallel_target_autoencode":[], "parallel_source-->target":[], "parallel_target-->source":[]}
+
+    # calculate masks if needed
+    if args.mask_output:
+        source_mask=np.where(src_input[:,:]>0,1,0.1)
+        target_mask=np.where(trg_input[:,:]>0,1,0.1)
+    else:
+        source_mask=None
+        target_mask=None
+
+    loss["parallel_source_autoencode"].append(model.source_to_source_model.test_on_batch(src_input, src_output, sample_weight=source_mask))
+    loss["parallel_target_autoencode"].append(model.target_to_target_model.test_on_batch(trg_input, trg_output, sample_weight=target_mask))
+    loss["parallel_source-->target"].append(model.source_to_target_model.test_on_batch(src_input, trg_output, sample_weight=target_mask))
+    loss["parallel_target-->source"].append(model.target_to_source_model.test_on_batch(trg_input, src_output, sample_weight=source_mask))
+
+    avg_loss=[(key,sum(value)/len(value)) if len(value)!=0 else (key,0.0) for key, value in sorted(loss.items(), key=lambda x:x[0])]
+#    print("devel loss:", sum([v for _,v in avg_loss]),avg_loss, flush=True)
+    return sum([v for _,v in avg_loss])
 
 def train(args):
             
@@ -73,26 +92,34 @@ def train(args):
     inf_iter=data_dense.infinite_iterator(src_f_name, trg_f_name, args.monolingual_source, args. monolingual_target)
     batch_iter=data_dense.fill_batch(args.minibatch_size, args.max_seq_len, vs, inf_iter)
 
-
-
-    # save model json
-#    model_json = encoder_decoder.to_json()
-#    with open(args.model_name+".json", "w") as json_file:
-#        json_file.write(model_json)
-
-    # callback to save weights after each epoch
-#    save_cb=ModelCheckpoint(filepath=args.model_name+".{epoch:02d}.h5", monitor='loss', verbose=1, save_best_only=True, mode='auto')
+    # devel data
+    source_devel="data/devel_data/newstest2015.fi.subwords"
+    target_devel="data/devel_data/newstest2015.en.subwords"
 
     
 
     counter=1
     loss={"extra_source_autoencode":[], "extra_target_autoencode":[], "parallel_source_autoencode":[], "parallel_target_autoencode":[], "parallel_source-->target":[], "parallel_target-->source":[]}
+    min_devel_loss=None
     while True:
 
+        # calculate devel loss and show example generation
         if counter%500==0:
             loss={"extra_source_autoencode":[], "extra_target_autoencode":[], "parallel_source_autoencode":[], "parallel_target_autoencode":[], "parallel_source-->target":[], "parallel_target-->source":[]}
-            evaluate_all(vs, encoder_decoder, next(batch_iter))
-            encoder_decoder.save(args.model_name)
+            devel_iter=data_dense.infinite_iterator(source_devel, target_devel, source_devel, target_devel, max_iterations=1)
+            devel_batch_iter=data_dense.fill_batch(16, args.max_seq_len, vs, devel_iter)
+            total_devel_loss=[]
+            random_number=np.random.randint(0,1000)
+            for i,batch in enumerate(devel_batch_iter):
+                if i==random_number:
+                    evaluate_all(vs, encoder_decoder, batch) # show one random example generation
+                total_devel_loss.append(calculate_devel_loss(encoder_decoder, batch, args))
+            total_devel_loss=sum(total_devel_loss)/len(total_devel_loss)
+            print("Development loss:",total_devel_loss)
+            if min_devel_loss==None or total_devel_loss<min_devel_loss:
+                print("Development loss decreased from {old} to {new}.".format(old=min_devel_loss, new=total_devel_loss), flush=True)
+                min_devel_loss=total_devel_loss
+                encoder_decoder.save(args.model_name)
 
         (mono_src_input, mono_src_output), (mono_trg_input, mono_trg_output), (src_input, trg_input, src_output, trg_output) = next(batch_iter)
 
@@ -108,7 +135,7 @@ def train(args):
             source_mask_mono=None
             target_mask_mono=None
  
-        # show example generation
+        # train on additional monolingual data
         if counter%100==0:
             # train on extra monolingual data
             loss["extra_source_autoencode"].append(encoder_decoder.source_to_source_model.train_on_batch(mono_src_input, mono_src_output, sample_weight=source_mask_mono))
@@ -124,20 +151,21 @@ def train(args):
         loss["parallel_source-->target"].append(encoder_decoder.source_to_target_model.train_on_batch(src_input, trg_output, sample_weight=target_mask))
         loss["parallel_target-->source"].append(encoder_decoder.target_to_source_model.train_on_batch(trg_input, src_output, sample_weight=source_mask))
 
-        # print loss
+        # print training loss
         if counter%50==0:
-            avg_loss=[(key,sum(value)/len(value)) if len(value)!=0 else (key,0.0) for key, value in loss.items()]
-            print("batch:", counter, "loss:", sum([v for _,v in avg_loss]),avg_loss, flush=True)
+            avg_loss=[(key,sum(value)/len(value)) if len(value)!=0 else (key,0.0) for key, value in sorted(loss.items(), key=lambda x:x[0])]
+            print("batch:", counter, "training loss:", sum([v for _,v in avg_loss]),avg_loss, flush=True)
 
         counter+=1
 
 
-        # TODO: save
 
         # check that embeddings are shared
         #print("src-to-src:",encoder_decoder.source_to_source_model.get_layer("model_1").get_layer("embeddings").get_weights()[0][5][:5])
         #print("src-to-trg:",encoder_decoder.source_to_target_model.get_layer("model_1").get_layer("embeddings").get_weights()[0][5][:5])
         #print("plain source encoder",encoder_decoder.source_encoder.get_layer("embeddings").get_weights()[0][5][:5])
+
+        #print("plain target encoder",encoder_decoder.target_encoder.get_layer("embeddings").get_weights()[0][5][:5])
         
         
 
